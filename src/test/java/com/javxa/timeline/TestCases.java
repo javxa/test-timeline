91fb24f567417b6e067a6fe23e7ec8f4383ffacb
Carl Caesar <carljfcaesar@gmail.com>
2024-01-23 23:43:12 +0100

    add another test case

--- src/test/java/com/javxa/timeline/TestCases.java
+++ src/test/java/com/javxa/timeline/TestCases.java
@@ -23,4 +23,48 @@ class TestCases {
         log.info("User logs out");
     }
 
+    /**
+     * The following test method will run once for every combination of divergences
+     */
+    @Test
+    void test_DinnerPreparations() {
+        if (Timeline.begin(this::test_DinnerPreparations)) return;
+
+        if (Timeline.diverge()) {
+            log.info("Decide to order food");
+
+            if (Timeline.diverge()) {
+                log.info("Order with Foodora");
+            } else {
+                log.info("Order with Uber eats");
+            }
+
+        } else {
+            log.info("Buy Groceries");
+            log.info("Prepare food");
+        }
+
+        if (Timeline.diverge()) {
+            log.info("Eat slowly..");
+        } else if (Timeline.diverge()) {
+            log.info("Eat in normal speed");
+        } else if (Timeline.diverge()) {
+            log.info("Eat fast");
+        } else {
+            log.info("Gulp that shit");
+        }
+
+        if (Timeline.diverge()) {
+            log.info("Do dishes");
+        } else {
+            log.info("Skip dishes");
+        }
+
+        if (Timeline.diverge()) {
+            log.info("Go to bed early");
+        } else {
+            log.info("Stay up late");
+        }
+    }
+
 }
